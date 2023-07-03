5fae5b19d084678140f4f6998f93ff667d71469e
Briss <alejandra@gmail.com>
2023-07-03 11:21:09 -0600

    pagina cambios

--- 04-proyeco-web/src/components/ProductCard.jsx
+++ 04-proyeco-web/src/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import { FaWhatsapp } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 const ProductCard = ({ id, title, description, url, price, quantity }) => {
   const grayscale = quantity === 0 ? { filter: 'grayscale(100%) brightness(60%)' } : {}
@@ -21,12 +22,12 @@ const ProductCard = ({ id, title, description, url, price, quantity }) => {
   return (
     <div className='col-md-3 my-3 d-flex align-items-stretch'>
       <div className='card'>
-        <a href={`/product/${id}`}>
+        <Link to={`/product/${id}`}>
           <div className='image-container'>
             <img className='product-image' src={`/assets/img/${url}`} alt={title} style={grayscale} />
             <div className={availabilityClass}>{availabilityText}</div>
           </div>
-        </a>
+        </Link>
         <div className='card-body d-flex flex-column justify-content-between'>
           <div>
             <h5 className='card-title'>{title}</h5>
--- 04-proyeco-web/src/pages/Product.jsx
+++ 04-proyeco-web/src/pages/Product.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
-
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 const Product = () => {
+  const myRequest = new Request('../assets/lentes.json')
+  const [product, setProduct] = useState({})
+  const productId = useParams().id /* guarda valor de id que esta en el route product/:id */
+  useEffect(() => {
+    fetch(myRequest)
+      .then((response) => {
+        return response.json()
+      }).then((glasses) => {
+        const oneProduct = glasses.find((item) => item.id === productId)
+        setProduct(oneProduct)
+      }).catch((error) => {
+        console.error(error)
+      })
+  }, [])
+
+  if (!product) {
+    return <div>Error:produco no encontrado</div>
+  }
+  /* productId saca solo el id con ayuda del useParam.id */
   return (
-    <div>Product</div>
+    <div>
+      <p>El ID del producto es {productId}</p>
+      <h2>Producto:{product.title}</h2>
+      <p>Description:{product.description}</p>
+      <h2>Precio: {product.price}</h2>
+
+    </div>
   )
 }
 
